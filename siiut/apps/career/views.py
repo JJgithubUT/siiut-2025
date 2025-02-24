3738bc4bc4dfbfd5404f930f51b5cd470caa2b74
JuanJavier <juanjcbreton@gmail.com>
2025-02-24 15:31:17 -0600

    24-2-25 ���

--- siiut/apps/career/views.py
+++ siiut/apps/career/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Quarter
+# CRUD  Views para Quarter
+def quarter_list(request):
+    quarters = Quarter.objects.all()
+    context = {
+        "quarters": quarters
+    }
+    return render(request, 'career/quarter/index.html', context)
 
-# Create your views here.
+def quarter_create(request, q_id):
+    return render(request, 'career/quarter/create.html')
+
+def quarter_details(request, q_id):
+    return render(request, 'career/quarter/details.html')
+
+def quarter_update(request, q_id):
+    return render(request, 'career/quarter/update.html')
+
+def quarter_delete(request, q_id):
+    return render(request, 'career/quarter/delete.html')
