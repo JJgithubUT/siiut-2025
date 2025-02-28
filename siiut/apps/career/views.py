f9ad4f3379a1b961f903f21348471759cee8a931
JuanJavier <juanjcbreton@gmail.com>
2025-02-28 09:51:18 -0600

    28-2-25

--- siiut/apps/career/views.py
+++ siiut/apps/career/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Quarter
+from .forms import QuarterForm
 # CRUD  Views para Quarter
 def quarter_list(request):
     quarters = Quarter.objects.all()
@@ -8,14 +9,28 @@ def quarter_list(request):
     }
     return render(request, 'career/quarter/index.html', context)
 
-def quarter_create(request, q_id):
-    return render(request, 'career/quarter/create.html')
+def quarter_create(request):
+    if request.method == 'POST':
+        form = QuarterForm(request.POST)
+        if form.is_valid():
+            quarter = Quarter(
+                quarter = form.cleaned_data['quarter'],
+                quarter_name = form.cleaned_data['quarter_name']
+            )
+            quarter.save()
+            return redirect('career:quarter_list')
+    else:
+        form = QuarterForm()
+        return render(request, 'career/quarter/create.html', {'form': form})
+
+    return render(request, 'career/quarter/create.html',{'form': form})
 
 def quarter_details(request, q_id):
-    return render(request, 'career/quarter/details.html')
+    quarter = Quarter.objects.get(pk=q_id)
+    return render(request, 'career/quarter/details.html', {'quarter': quarter})
 
 def quarter_update(request, q_id):
     return render(request, 'career/quarter/update.html')
 
 def quarter_delete(request, q_id):
-    return render(request, 'career/quarter/delete.html')
+    pass
